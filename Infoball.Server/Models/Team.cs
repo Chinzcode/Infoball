fa1dee191ea456c7290066beb12dae92b97e7b37
Chinzcode <trym.ss98@gmail.com>
2025-05-29 00:24:12 +0200

    feat(data): add Team model and database context
    - Add Team Entity with basic properties
    - Create ApplicationDbContext with DbSets
    - Setup dummy database connection string. Will update this later

---
+++ Infoball.Server/Models/Team.cs
@@ -0,0 +1,11 @@
+namespace Infoball.Server.Models;
+
+/// <summary>
+/// Team Model
+/// </summary>
+public class Team
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? League { get; set; }
+}
