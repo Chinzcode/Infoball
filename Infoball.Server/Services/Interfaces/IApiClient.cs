2a56ecfb7a43beec6c437c7c67cc59cb3955cb61
Chinzcode <trym.ss98@gmail.com>
2025-06-03 00:03:19 +0200

    fix(api): add constraint to FetchDataAsync in IApiClient

--- Infoball.Server/Services/Interfaces/IApiClient.cs
+++ Infoball.Server/Services/Interfaces/IApiClient.cs
@@ -3,5 +3,6 @@ namespace Infoball.Server.Services.Interfaces;
 public interface IApiClient
 {
     Task<string> FetchRawDataAsync(Dictionary<string, string> queryParams);
-    Task<T?> FetchDataAsync<T>(Dictionary<string, string> queryParams);
+    Task<T?> FetchDataAsync<T>(Dictionary<string, string> queryParams) where T : class;
+
 }
