2e740a19773ca6e61b923317a63e1f582c0cd0a7
Chinzcode <trym.ss98@gmail.com>
2025-06-10 13:07:13 +0200

    fix(models): update Standing Domain

--- Infoball.Shared/Models/Domain/Standing.cs
+++ Infoball.Shared/Models/Domain/Standing.cs
@@ -4,15 +4,31 @@ namespace Infoball.Shared.Models.Domain;
 
 public class Standing
 {
-    public int Id { get; set; }
-    public string League { get; set; }
+    public int Rank { get; set; }
+    public int TeamId { get; set; }
+    public int Points { get; set; }
+    public int MatchesPlayed { get; set; }
+    public int Wins { get; set; }
+    public int Draws { get; set; }
+    public int Losses { get; set; }
+    public int GoalsFor { get; set; }
+    public int GoalsAgainst { get; set; }
+    public int GoalsDifference { get; set; }
 
     public StandingDTO ToDto()
     {
         return new StandingDTO
         {
-            Id = Id,
-            League = League
+            Rank = Rank,
+            TeamId = TeamId,
+            Points = Points,
+            MatchesPlayed = MatchesPlayed,
+            Wins = Wins,
+            Draws = Draws,
+            Losses = Losses,
+            GoalsFor = GoalsFor,
+            GoalsAgainst = GoalsAgainst,
+            GoalsDifference = GoalsDifference
         };
     }
 }
