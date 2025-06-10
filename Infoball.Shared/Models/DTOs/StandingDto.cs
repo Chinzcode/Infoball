6fee7917453280f1406d81d08f27098fd352c715
Chinzcode <trym.ss98@gmail.com>
2025-06-10 13:06:46 +0200

    feat(models): add Standing DTO

---
+++ Infoball.Shared/Models/DTOs/StandingDto.cs
@@ -0,0 +1,15 @@
+namespace Infoball.Shared.Models.DTOs;
+
+public class StandingDTO
+{
+    public int Rank { get; set; }
+    public int TeamId { get; set; }
+    public int Points { get; set; }
+    public int MatchesPlayed { get; set; }
+    public int Wins { get; set; }
+    public int Draws { get; set; }
+    public int Losses { get; set; }
+    public int GoalsFor { get; set; }
+    public int GoalsAgainst { get; set; }
+    public int GoalsDifference { get; set; }
+}
