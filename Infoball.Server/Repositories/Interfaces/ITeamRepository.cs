eb90e9ba76b2fd1645746d0d8dd3f185dc8166c5
Chinzcode <trym.ss98@gmail.com>
2025-06-11 17:39:16 +0200

    feat(team): add SaveTeamAsync method to ITeamRepository

--- Infoball.Server/Repositories/Interfaces/ITeamRepository.cs
+++ Infoball.Server/Repositories/Interfaces/ITeamRepository.cs
@@ -5,8 +5,5 @@ namespace Infoball.Server.Repositories.Interfaces;
 public interface ITeamRepository
 {
     Task<Team?> GetTeamByIdAsync(int id);
-    // Task<List<Team>> GetTeamsAsync();
-    // Task<Team> CreateTeamAsync(Team team);
-    // Task<Team> UpdateTeamAsync(Team team);
-    // Task<bool> DeleteTeamAsync(int id);
+    Task SaveTeamAsync(Team team);
 }
